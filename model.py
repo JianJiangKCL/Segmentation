e52047146bc777471482f24f13b6d7c4362fc16e
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2020-12-31 17:31:01 +0000

    a single decoder, a projection head added to the output of decoder.

---
+++ model.py
@@ -0,0 +1,218 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.nn.functional import normalize
+
+def conv_block( in_ch, out_ch):
+	return nn.Sequential(nn.Conv2d(in_ch, out_ch, 3, 1, 1),
+						 nn.BatchNorm2d(out_ch),
+						 nn.ReLU(inplace=True),
+						 nn.Conv2d(out_ch, out_ch, 3, 1, 1),
+						 nn.BatchNorm2d(out_ch),
+						 nn.ReLU(inplace=True),
+						 )
+
+
+def up_block(in_ch, out_ch):
+	return nn.Sequential(
+		nn.ConvTranspose2d(in_ch, out_ch, 2, 2),
+		nn.BatchNorm2d(out_ch),
+		nn.ReLU(inplace=True)
+	)
+
+
+class Decoder(nn.Module):
+	def __init__(self):
+		super(Decoder, self).__init__()
+		kernels = 32
+		self.up_conv6 = up_block(kernels * 16, kernels * 8)
+		self.conv6 = conv_block(kernels * 16, kernels * 8)
+		self.up_conv7 = up_block(kernels * 8, kernels * 4)
+		self.conv7 = conv_block(kernels * 8, kernels * 4)
+		self.up_conv8 = up_block(kernels * 4, kernels * 2)
+		self.conv8 = conv_block(kernels * 4, kernels * 2)
+		self.up_conv9 = up_block(kernels * 2, kernels)
+		self.conv9 = conv_block(kernels * 2, kernels)
+
+		self.conv10 = nn.Conv2d(kernels, 1, 1)
+
+	def forward(self, latents):
+		x1, x2, x3, x4, x5 = latents
+		x6 = self.up_conv6(x5)
+		c6 = torch.cat((x6, x4), dim=1)
+		h6 = self.conv6(c6)
+		#         (128, 32, 32) -> (64, 64, 64)
+		x7 = self.up_conv7(h6)
+		c7 = torch.cat((x7, x3), dim=1)
+		h7 = self.conv7(c7)
+		#         (64, 64, 64) -> (32, 128, 128)
+		x8 = self.up_conv8(h7)
+		c8 = torch.cat((x8, x2), dim=1)
+		h8 = self.conv8(c8)
+		#         (32, 128, 128) -> (16, 256, 256)
+		x9 = self.up_conv9(h8)
+		c9 = torch.cat((x9, x1), dim=1)
+		h9 = self.conv9(c9)
+		#         (16, 256, 256) -> (1, 256, 256)
+		h10 = self.conv10(h9)
+		out = F.sigmoid(h10)
+		return out
+class projection_MLP(nn.Module):
+	def __init__(self, in_dim, hidden_dim=1024, out_dim=256):
+		super().__init__()
+		''' page 3 baseline setting
+		Projection MLP. The projection MLP (in f) has BN ap-
+		plied to each fully-connected (fc) layer, including its out- 
+		put fc. Its output fc has no ReLU. The hidden fc is 2048-d. 
+		This MLP has 3 layers.
+		'''
+		self.layer1 = nn.Sequential(
+			nn.Linear(in_dim, hidden_dim),
+			nn.BatchNorm1d(hidden_dim),
+			nn.ReLU(inplace=True)
+		)
+		self.layer2 = nn.Sequential(
+			nn.Linear(hidden_dim, hidden_dim),
+			nn.BatchNorm1d(hidden_dim),
+			nn.ReLU(inplace=True)
+		)
+		self.layer3 = nn.Sequential(
+			nn.Linear(hidden_dim, out_dim),
+			nn.BatchNorm1d(out_dim)
+		)
+		self.num_layers = 3
+	def set_layers(self, num_layers):
+		self.num_layers = num_layers
+
+	def forward(self, x):
+		if self.num_layers == 3:
+			x = self.layer1(x)
+			x = self.layer2(x)
+			x = self.layer3(x)
+		elif self.num_layers == 2:
+			x = self.layer1(x)
+			x = self.layer3(x)
+		else:
+			raise Exception
+		x = F.sigmoid(x)
+		return x
+class UNet(nn.Module):
+
+	def __init__(self):
+		super(UNet, self).__init__()
+
+		kernels = 32
+
+		self.conv1 = self.conv_block(3, kernels)
+		self.conv2 = self.conv_block(kernels, kernels * 2)
+		self.conv3 = self.conv_block(kernels * 2, kernels * 4)
+		self.conv4 = self.conv_block(kernels * 4, kernels * 8)
+		self.conv5 = self.conv_block(kernels * 8, kernels * 16)
+
+		self.down_pooling = nn.MaxPool2d(2)
+
+		self.fore_decoder = Decoder()
+		self.back_decoder = Decoder()
+		self.add_module('Foreground_decoder', self.fore_decoder)
+		self.add_module('background_decoder', self.back_decoder)
+		# self.up_conv6 = self.up_block(kernels * 16, kernels * 8)
+		# self.conv6 = self.conv_block(kernels * 16, kernels * 8)
+		# self.up_conv7 = self.up_block(kernels * 8, kernels * 4)
+		# self.conv7 = self.conv_block(kernels * 8, kernels * 4)
+		# self.up_conv8 = self.up_block(kernels * 4, kernels * 2)
+		# self.conv8 = self.conv_block(kernels * 4, kernels * 2)
+		# self.up_conv9 = self.up_block(kernels * 2, kernels)
+		# self.conv9 = self.conv_block(kernels * 2, kernels)
+		#
+		# self.conv10 = nn.Conv2d(kernels, 1, 1)
+		self.proj5 = projection_MLP(16384, 4096, 16384)
+		self._initialize_weights()
+
+	def up_block(self, in_ch, out_ch):
+		return nn.Sequential(
+			nn.ConvTranspose2d(in_ch, out_ch, 2, 2),
+			nn.BatchNorm2d(out_ch),
+			nn.ReLU(inplace=True)
+		)
+
+	def conv_block(self, in_ch, out_ch):
+		return nn.Sequential(nn.Conv2d(in_ch, out_ch, 3, 1, 1),
+							 nn.BatchNorm2d(out_ch),
+							 nn.ReLU(inplace=True),
+							 nn.Conv2d(out_ch, out_ch, 3, 1, 1),
+							 nn.BatchNorm2d(out_ch),
+							 nn.ReLU(inplace=True),
+							 )
+
+	def forward(self, x):
+		#         Encoder Part
+		#         (3, 256, 256) -> (16, 128, 128)
+		x1 = self.conv1(x)
+		h1 = self.down_pooling(x1)
+		#         (16, 128, 128) -> (32, 64, 64)
+		x2 = self.conv2(h1)
+		h2 = self.down_pooling(x2)
+		#         (32, 64, 64) -> (64, 32, 32)
+		x3 = self.conv3(h2)
+		h3 = self.down_pooling(x3)
+		#         (64, 32, 32) -> (128, 16, 16)
+		x4 = self.conv4(h3)
+		h4 = self.down_pooling(x4)
+
+		#         (128, 16, 16) -> (256, 16, 16)
+		# 512x8x8
+		x5 = self.conv5(h4)
+
+
+		#         Decoder Part
+		#         (256, 16, 16) -> (128, 32, 32)
+		# x6 = self.up_conv6(x5)
+		# c6 = torch.cat((x6, x4), dim=1)
+		# h6 = self.conv6(c6)
+		# #         (128, 32, 32) -> (64, 64, 64)
+		# x7 = self.up_conv7(h6)
+		# c7 = torch.cat((x7, x3), dim=1)
+		# h7 = self.conv7(c7)
+		# #         (64, 64, 64) -> (32, 128, 128)
+		# x8 = self.up_conv8(h7)
+		# c8 = torch.cat((x8, x2), dim=1)
+		# h8 = self.conv8(c8)
+		# #         (32, 128, 128) -> (16, 256, 256)
+		# x9 = self.up_conv9(h8)
+		# c9 = torch.cat((x9, x1), dim=1)
+		# h9 = self.conv9(c9)
+		# #         (16, 256, 256) -> (1, 256, 256)
+		# h10 = self.conv10(h9)
+		# out = F.sigmoid(h10)
+		latents = (x1, x2, x3, x4, x5)
+		fore_out = self.fore_decoder(latents)
+		size = fore_out.size()
+		fore_out = fore_out.view(size[0], -1)
+		back_out = self.proj5(fore_out.detach()).flatten()
+		fore_out = fore_out.flatten()
+		# back_out = self.back_decoder(latents).flatten()
+		# ph_out = torch.zeros_like(fore_out)
+		indices = (fore_out < back_out).nonzero()
+		ph_out = fore_out + (1 - back_out)
+		ph_out[indices] = ph_out[indices] - fore_out[indices]
+		back_indices = (fore_out > back_out).nonzero()
+		ph_out[back_indices] = ph_out[back_indices] + back_out[back_indices] - 1
+		# print('---------------------')
+		# print(indices.size())
+		# print(fore_out.size())
+		ph_out = ph_out.reshape(*size)
+		return ph_out
+
+	def _initialize_weights(self):
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+				if m.bias is not None:
+					nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.BatchNorm2d):
+				nn.init.constant_(m.weight, 1)
+				nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.Linear):
+				nn.init.normal_(m.weight, 0, 0.01)
+				nn.init.constant_(m.bias, 0)
---
+++ train.py
@@ -0,0 +1,193 @@
+from skimage import io, transform
+from sklearn.model_selection import train_test_split
+import argparse
+import glob
+import matplotlib.pyplot as plt
+import random
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+import albumentations as A
+from albumentations.pytorch import ToTensor
+from tqdm import tqdm as tqdm
+from model import UNet
+from albumentations import (HorizontalFlip, ShiftScaleRotate, Normalize, Resize, Compose, GaussNoise)
+
+
+class NucleiDataset(Dataset):
+	def __init__(self, root, folders, transform=None):
+		self.root = root
+		self.folders = folders
+		self.transforms = transform
+
+	def __len__(self):
+		return len(self.folders)
+
+	def __getitem__(self, idx):
+		image_folder = os.path.join(self.root, self.folders[idx], 'images/')
+		mask_folder = os.path.join(self.root, self.folders[idx], 'masks/')
+
+		image_path = os.path.join(image_folder, os.listdir(image_folder)[0])
+		img = io.imread(image_path)[:, :, :3].astype('float32')
+		mask = self.get_mask(mask_folder).astype('uint8')
+
+		augmented = self.transforms(image=img, mask=mask)
+		img, mask = augmented['image'], augmented['mask']
+		return (img, mask)
+
+	def get_mask(self, mask_folder):
+		masks = io.imread_collection(mask_folder + '*.png').concatenate()
+		mask = np.max(masks, axis=0)
+		return mask
+
+
+class DiceLoss(nn.Module):
+	def __init__(self):
+		super(DiceLoss, self).__init__()
+
+	def forward(self, input, target):
+		N = target.size(0)
+		smooth = 1.
+
+		input_flat = input.view(N, -1)
+		target_flat = target.view(N, -1)
+
+		intersection = input_flat * target_flat
+
+		loss = 2 * (intersection.sum(1) + smooth) / (input_flat.sum(1) + target_flat.sum(1) + smooth)
+		loss = 1 - loss.sum() / N
+
+		return loss
+
+
+class DiceBCELoss(nn.Module):
+	def __init__(self):
+		super(DiceBCELoss, self).__init__()
+		self.bce_ratio = 0.5
+		self.bce_criterion = nn.BCELoss().cuda()
+		self.dice_criterion = DiceLoss().cuda()
+
+	def forward(self, input, target):
+		bce_loss = self.bce_criterion(input, target)
+		dice_loss = self.dice_criterion(input, target)
+		return bce_loss * self.bce_ratio + dice_loss
+
+
+def compute_iou(y_true, y_pred):
+	eps = 1e-6
+	y_true = y_true > 0.5
+	y_pred = y_pred > 0.5
+
+	intersection = (y_true & y_pred).float().sum((1, 2, 3))
+	union = (y_true | y_pred).float().sum((1, 2, 3))
+
+	iou = (intersection + eps) / (union + eps)
+	return iou
+
+
+def train(loader, model, opt, criterion):
+	avg_loss = 0.0
+	avg_iou = 0.0
+
+	model.train()
+	for i, (imgs, masks) in enumerate(loader):
+		imgs, masks = imgs.cuda(), masks.cuda()
+		opt.zero_grad()
+		pred = model(imgs)
+		loss = criterion(pred, masks)
+		iou = compute_iou(masks.data, pred.data)
+		loss.backward()
+		opt.step()
+		avg_loss += loss.item() * len(masks)
+		avg_iou += iou.sum().item()
+
+	avg_loss /= len(loader.dataset)
+	avg_iou /= len(loader.dataset)
+	return avg_loss, avg_iou
+
+
+def valid(loader, model, opt, criterion):
+	avg_loss = 0.0
+	avg_iou = 0.0
+	model.eval()
+
+	with torch.no_grad():
+		for i, (imgs, masks) in enumerate(loader):
+			imgs, masks = imgs.cuda(), masks.cuda()
+			pred = model(imgs)
+			loss = criterion(pred, masks)
+			iou = compute_iou(masks.data, pred.data)
+			avg_loss += loss.item() * len(masks)
+			avg_iou += iou.sum().item()
+
+	avg_loss /= len(loader.dataset)
+	avg_iou /= len(loader.dataset)
+	return avg_loss, avg_iou
+
+def main(args):
+	img_paths = os.listdir(args.dataset)
+	train_paths, valid_paths = train_test_split(img_paths, test_size=args.split_ratio, random_state=args.random_seed)
+
+	train_dataset = NucleiDataset(args.dataset, train_paths, A.Compose([
+		A.Resize(args.img_size, args.img_size),
+		A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+		A.HorizontalFlip(p=0.25),
+		A.VerticalFlip(p=0.25),
+		ToTensor()]))
+
+	valid_dataset = NucleiDataset(args.dataset, valid_paths, A.Compose([
+		A.Resize(args.img_size, args.img_size),
+		A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+		ToTensor()]))
+
+	trainloader = torch.utils.data.DataLoader(train_dataset,
+	                                          batch_size=args.batch_size,
+	                                          shuffle=True,
+	                                          num_workers=0,
+	                                          pin_memory=True)
+
+	validloader = torch.utils.data.DataLoader(valid_dataset,
+	                                          batch_size=args.batch_size,
+	                                          shuffle=False,
+	                                          num_workers=0,
+	                                          pin_memory=True)
+
+
+	image, mask = train_dataset.__getitem__(0)
+	print(image.shape)
+	print(mask.shape)
+	unet = UNet().cuda()
+	opt = torch.optim.Adam(unet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+	# BCE_criterion = nn.BCELoss().cuda()
+	# DICE_criterion = DiceLoss().cuda()
+	DICE_BCE_criterion = DiceBCELoss().cuda()
+
+	for i in range(1, args.n_epochs + 1):
+		train_loss, train_iou = train(trainloader, unet, opt, DICE_BCE_criterion)
+		valid_loss, valid_iou = valid(validloader, unet, opt, DICE_BCE_criterion)
+		print('Epoch:{:02d}/{:02d}  Loss:{:.3f} IOU:{:.3f}  Valid_Loss:{:.3f} Valid_IOU:{:.3f}'.format(
+			i, args.n_epochs, train_loss, train_iou, valid_loss, valid_iou))
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dataset', type=str, default='D:\Dataset\data-science-bowl-2018\stage1_train')
+	parser.add_argument('--img_size', type=int, default=128)
+	parser.add_argument('--n_epochs', type=int, default=100)
+	parser.add_argument('--batch_size', type=int, default=16)
+	parser.add_argument('--lr', type=float, default=3e-4)
+	parser.add_argument('--weight_decay', type=float, default=1e-4)
+
+	parser.add_argument('--split_ratio', type=float, default=0.2)
+
+	parser.add_argument('--random_seed', type=float, default=42)
+
+	args, _ = parser.parse_known_args()
+
+
+
+	main(args)
